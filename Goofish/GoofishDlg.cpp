f629f0da5050e983f7cf67cee03b5bf07bc15ae1
Jacob <2517917168@qq.com>
2025-11-15 20:12:50 +0800

    简化CGoofishDlg类，移除多余消息处理函数
    移除了CGoofishDlg类中的OnSysCommand、OnPaint和
    OnQueryDragIcon消息处理函数及其相关代码，清理了
    GoofishDlg.h中的对应声明和消息映射。保留了核心
    功能，包括控件绑定和按钮点击处理逻辑。新增了
    OnClose函数，调用KillSelfProcess实现自定义关闭逻辑。

--- Goofish/GoofishDlg.cpp
+++ Goofish/GoofishDlg.cpp
@@ -39,9 +39,6 @@ void CGoofishDlg::DoDataExchange(CDataExchange* pDX)
 }
 
 BEGIN_MESSAGE_MAP(CGoofishDlg, CDialog)
-	ON_WM_SYSCOMMAND()
-	ON_WM_PAINT()
-	ON_WM_QUERYDRAGICON()
 	ON_WM_CLOSE()
 	ON_BN_CLICKED(IDC_BUTTON_CONNECT, &CGoofishDlg::OnBnClickedButtonConnect)
 	ON_BN_CLICKED(IDC_BUTTON_SEND, &CGoofishDlg::OnBnClickedButtonSend)
@@ -76,47 +73,6 @@ BOOL CGoofishDlg::OnInitDialog()
 		});
 
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
-}
-
-void CGoofishDlg::OnSysCommand(UINT nID, LPARAM lParam)
-{
-	CDialog::OnSysCommand(nID, lParam);
-}
-
-// 如果向对话框添加最小化按钮，则需要下面的代码
-//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
-//  这将由框架自动完成。
-
-void CGoofishDlg::OnPaint()
-{
-	if (IsIconic())
-	{
-		CPaintDC dc(this); // 用于绘制的设备上下文
-
-		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
-
-		// 使图标在工作区矩形中居中
-		int cxIcon = GetSystemMetrics(SM_CXICON);
-		int cyIcon = GetSystemMetrics(SM_CYICON);
-		CRect rect;
-		GetClientRect(&rect);
-		int x = (rect.Width() - cxIcon + 1) / 2;
-		int y = (rect.Height() - cyIcon + 1) / 2;
-
-		// 绘制图标
-		dc.DrawIcon(x, y, m_hIcon);
-	}
-	else
-	{
-		CDialog::OnPaint();
-	}
-}
-
-//当用户拖动最小化窗口时系统调用此函数取得光标
-//显示。
-HCURSOR CGoofishDlg::OnQueryDragIcon()
-{
-	return static_cast<HCURSOR>(m_hIcon);
 }
 
 void CGoofishDlg::OnClose()
